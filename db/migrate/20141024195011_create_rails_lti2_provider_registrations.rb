7d5cccc398d08af906e39ab5b19f3e588a845cde
Bill Zhang <billzzhang2@gmail.com>
2020-07-24 14:02:36 -0400

    LTI-42: Add rubocop and automated test with test with travis to LTI2Provider gem (#1)
    * initial travis file
    
    * travis changes
    
    * gemfile add rubocop
    
    * bundle install
    
    * rubocop autocorrect
    
    * rubocop no offenses
    
    * add manifest.js file
    
    * change manifest.js
    
    * changed travis build from rspec to rails test
    
    * no db
    
    * rubocop frozen_string_literal

--- db/migrate/20141024195011_create_rails_lti2_provider_registrations.rb
+++ db/migrate/20141024195011_create_rails_lti2_provider_registrations.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CreateRailsLti2ProviderRegistrations < ActiveRecord::Migration[4.2]
   def change
     create_table :rails_lti2_provider_registrations do |t|
--- db/migrate/20150817183400_update_tool_proxy_shared_secret.rb
+++ db/migrate/20150817183400_update_tool_proxy_shared_secret.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class UpdateToolProxySharedSecret < ActiveRecord::Migration[4.2]
   def change
     change_column :rails_lti2_provider_tools, :shared_secret, :text, limit: nil
