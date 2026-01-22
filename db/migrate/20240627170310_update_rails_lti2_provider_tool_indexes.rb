2edd618099eda3cd3d36f2e3b51e8cf4d134db3c
Mariam05 <tomariam@gmail.com>
2026-01-22 16:08:35 -0500

    fix failing migration

--- db/migrate/20240627170310_update_rails_lti2_provider_tool_indexes.rb
+++ db/migrate/20240627170310_update_rails_lti2_provider_tool_indexes.rb
@@ -1,21 +1,42 @@
 # frozen_string_literal: true
 
 class UpdateRailsLti2ProviderToolIndexes < ActiveRecord::Migration[6.1]
-  def self.up
-    remove_index('rails_lti2_provider_tools', name: 'index_tool_id_tenant_id')
-    remove_index('rails_lti2_provider_tools', name: 'index_tenant_id')
-    remove_index('rails_lti2_provider_tools', name: 'index_tenant_id_uuid')
-    add_index('rails_lti2_provider_tools', %w[id], name: 'index_id')
-    add_index('rails_lti2_provider_tools', %w[uuid], name: 'index_uuid')
-    add_index('rails_lti2_provider_tools', %w[uuid shared_secret], name: 'index_uuid_shared_secret')
+  def up
+    # Old indexes may or may not exist depending on DB history.
+    remove_index :rails_lti2_provider_tools, name: "index_tool_id_tenant_id", if_exists: true
+    remove_index :rails_lti2_provider_tools, name: "index_tenant_id",         if_exists: true
+    remove_index :rails_lti2_provider_tools, name: "index_tenant_id_uuid",    if_exists: true
+
+    # New indexes: guard against environments where they already exist.
+    add_index :rails_lti2_provider_tools, :id,
+              name: "index_id",
+              if_not_exists: true
+
+    add_index :rails_lti2_provider_tools, :uuid,
+              name: "index_uuid",
+              if_not_exists: true
+
+    add_index :rails_lti2_provider_tools, [:uuid, :shared_secret],
+              name: "index_uuid_shared_secret",
+              if_not_exists: true
   end
 
-  def self.down
-    remove_index('rails_lti2_provider_tools', name: 'index_uuid_shared_secret')
-    remove_index('rails_lti2_provider_tools', name: 'index_uuid')
-    remove_index('rails_lti2_provider_tools', name: 'index_id')
-    add_index('rails_lti2_provider_tools', %w[tenant_id uuid], name: 'index_tenant_id_uuid', unique: true)
-    add_index('rails_lti2_provider_tools', %w[tenant_id], name: 'index_tenant_id')
-    add_index('rails_lti2_provider_tools', %w[id tenant_id], name: 'index_tool_id_tenant_id')
+  def down
+    remove_index :rails_lti2_provider_tools, name: "index_uuid_shared_secret", if_exists: true
+    remove_index :rails_lti2_provider_tools, name: "index_uuid",              if_exists: true
+    remove_index :rails_lti2_provider_tools, name: "index_id",                if_exists: true
+
+    add_index :rails_lti2_provider_tools, [:tenant_id, :uuid],
+              name: "index_tenant_id_uuid",
+              unique: true,
+              if_not_exists: true
+
+    add_index :rails_lti2_provider_tools, :tenant_id,
+              name: "index_tenant_id",
+              if_not_exists: true
+
+    add_index :rails_lti2_provider_tools, [:id, :tenant_id],
+              name: "index_tool_id_tenant_id",
+              if_not_exists: true
   end
 end
--- lib/rails_lti2_provider/version.rb
+++ lib/rails_lti2_provider/version.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 module RailsLti2Provider
-  VERSION = '0.2.1.3'
+  VERSION = '0.2.1.4'
 end
